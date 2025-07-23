7fe0f7b05f05636e7ddbd9cef5f0e5091965c270
Mpaulinaat <mariapaulina.amayat@gmail.com>
2025-07-23 14:26:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.querySelectorAll('.ramo').forEach(ramo => {
+  const id = ramo.dataset.id;
 
+  if (localStorage.getItem(id)) {
+    ramo.classList.add('aprobado');
+  }
+
+  ramo.addEventListener('click', () => {
+    if (ramo.classList.contains('bloqueado')) return;
+
+    ramo.classList.toggle('aprobado');
+
+    if (ramo.classList.contains('aprobado')) {
+      localStorage.setItem(id, true);
+    } else {
+      localStorage.removeItem(id);
+    }
+  });
+});
